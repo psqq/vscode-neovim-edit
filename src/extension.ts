761c74d26f24c4411894ea232b410fd8add3ea53
Sergey Pestov <pestov-sa@ya.ru>
2023-09-10 13:12:51 +0300

    Added repository information

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('norg.helloWorld', () => {
 		const filename = vscode.window.activeTextEditor?.document.fileName;
@@ -22,5 +18,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() { }
