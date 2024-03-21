c12988269dce343a6e3ca3f7f975e586a669351a
Philip Nelson <philipnelson5@gmail.com>
2024-03-21 00:39:30 -0600

    Open neovim to the same cursor position as vscode
    I love the simplicity of this extension. I tried two

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,19 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('nvim-edit.edit-file', () => {
-		const filename = vscode.window.activeTextEditor?.document.fileName;
-		if (!filename) {
-			vscode.window.showInformationMessage('No filename for open');
+		const editor = vscode.window.activeTextEditor
+		if (!editor) {
+			vscode.window.showInformationMessage('No active editor.');
 			return;
 		}
+		const filename = editor.document.fileName;
+		const cursor = editor.selection.active;
 		let terminal = vscode.window.createTerminal({
 			name: "nvim " + filename,
 			hideFromUser: false
 		});
 		terminal.show();
-		terminal.sendText('nvim ' + filename);
+		terminal.sendText(`nvim +'call cursor(${cursor.line+1},${cursor.character+1})' ${filename}`);
 	});
 
 	context.subscriptions.push(disposable);
